3c9d5355192ed7cf5e2c9926eb20676c61b1c692
Itay Meitav <itai9999@gmail.com>
2022-12-24 16:39:00 +0000

    Added UI searchbox

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,16 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import Searchbox from "./Searchbox";
 
-const Banner = () => {
+const Banner = (props) => {
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
+    <div style={{width: "100%"}} className="banner text-white">
+      <div style={{width: "100%"}} className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
+        <div style={{width: "100%", gap: "30px"}} className="d-flex flex-row justify-content-center align-items-center">
+          <span style={{minWidth: "max-content"}}>A place to get</span>
+          <Searchbox onTitleSearch={props.onTitleSearch}  />
+          <span  style={{minWidth: "max-content"}}>the cool stuff.</span>
         </div>
       </div>
     </div>
